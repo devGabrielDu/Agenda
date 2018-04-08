0471fde36bead9d5d587e1c115ecafb25c74fd77
devGabrielDu <devdutratruta@gmail.com>
2018-04-08 17:11:00 -0300

    Arrumando erros

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :contatos
+resources :contatos
+
+root 'contatos#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
